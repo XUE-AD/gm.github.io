b7ce2201bfe0beb530860a6b6fb38447dafc5d36
AD <adam@teachcake.com>
2024-07-30 03:36:59 +0800

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,10 +1,10 @@
 const firebaseApp = firebase.initializeApp({
-  apiKey: secrets.REACT_APP_FIREBASE_API_KEY,
-  authDomain: secrets.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: secrets.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: secrets.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: secrets.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: secrets.REACT_APP_FIREBASE_APP_ID,
-  measurementId: secrets.REACT_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: secrets.FIREBASE_API_KEY,
+  authDomain: secrets.FIREBASE_AUTH_DOMAIN,
+  projectId: secrets.FIREBASE_PROJECT_ID,
+  storageBucket: secrets.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: secrets.FIREBASE_MESSAGING_SENDER_ID,
+  appId: secrets.FIREBASE_APP_ID,
+  measurementId: secrets.FIREBASE_MEASUREMENT_ID
 });
 const auth = firebaseApp.auth();

07725bf9959153c23d603be25e040e8e8ffbefc7
AD <adam@teachcake.com>
2024-07-29 20:42:48 +0800

    update deploy

---
+++ test.js
@@ -0,0 +1,7 @@
+console.log(process.env)
+// authDomain: env.FIREBASE_AUTH_DOMAIN,
+// projectId: env.FIREBASE_PROJECT_ID,
+// storageBucket: env.FIREBASE_STORAGE_BUCKET,
+// messagingSenderId: env.FIREBASE_MESSAGING_SENDER_ID,
+// appId: env.FIREBASE_APP_ID,
+// measurementId: env.FIREBASE_MEASUREMENT_ID
